47f65f48896ca3466b3283a065f6858c8ea6a081
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-06-30 20:30:42 +0530

    Add files via upload

---
+++ Assignment 28.06.2022/blood_doante.java
@@ -0,0 +1,35 @@
+/*
+@Author hadiuzzaman
+*/
+
+// Find eligibility for donating blood. (age>=25, weight>=48)
+
+import java.util.Scanner;    // scanner package
+
+public class blood_doante
+{
+
+	public static void main(String[] args) 
+   // main method
+    {   
+     
+		Scanner input1 = new Scanner(System.in);    // create a scanner object
+		System.out.print("Enter your age: ");
+		int age = input1.nextInt();     // read 1st user input
+		
+		Scanner input2 = new Scanner(System.in);    // create a scanner object
+		System.out.print("Enter your weight: ");
+		int weight = input2.nextInt();  // read 2nd user input
+		
+		if(age>=25 && weight>=48) 
+        {           // 1st condition
+			System.out.println("You are eligible for Donating blood.");
+		}
+		else 
+        {                                // last condition
+			System.out.println("You are not eligible Donating blood.");
+		}
+
+	}
+
+}
